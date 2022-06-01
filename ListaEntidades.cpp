7ae688ce93982c6a668f16c4a22497795d2d0d18
Scalcon <pedroscalcon0730@gmail.com>
2022-06-01 12:41:27 -0300

    Update: Lista de Entidades que sera posteriormente chamada na fase

---
+++ ListaEntidades.cpp
@@ -0,0 +1,110 @@
+#include "ListaEntidades.h"
+//métodos lista entidades
+ListaEntidades::ListaEntidades() : current{ nullptr }, head{ nullptr }, tail{ nullptr } {}
+
+ListaEntidades::~ListaEntidades() {
+	destroyAll();
+}
+
+void ListaEntidades::insert(Entidade* info) {
+	if (info) {
+		ElementoLista* elem = new ElementoLista(info);
+		if (!head) {
+			head = elem;
+			tail = elem;
+		} else {
+			tail->setNext(elem);
+			elem->setPrev(tail);
+
+			tail = elem;
+		}
+	}
+}
+
+void ListaEntidades::deleteAll() {
+	ElementoLista* aux = head;
+	current = head;
+
+	while (current != nullptr) {
+		aux = current->getNext();
+		delete current;
+
+		current = aux;
+	}
+
+	head = nullptr;
+	current = nullptr;
+	tail = nullptr;
+
+}
+
+Entidade* ListaEntidades::moveHead() {
+	current = head;
+	return current ? current->getInfo() : nullptr;
+}
+
+Entidade* ListaEntidades::moveNext() {
+	current = current->getNext();
+	return current ? current->getInfo() : nullptr;
+}
+
+void ListaEntidades::atualizarEntidades(float t) {
+	Entidade* e = moveHead();
+
+	while (e) {
+		e->atualizar(t);
+		e = moveNext();
+	}
+}
+
+void ListaEntidades::drawAll(RenderWindow* window) {
+	Entidade* e = moveHead();
+
+	while (e) {
+		e->draw(window);
+		e = moveNext();
+	}
+}
+
+void ListaEntidades::destroyAll() {
+	Entidade* e = moveHead();
+
+	while (e) {
+		delete e;
+		e = moveNext();
+	}
+
+	deleteAll();
+}
+
+//métodos da elemento lista
+ListaEntidades::ElementoLista::ElementoLista(Entidade* Info, ElementoLista* Prev, ElementoLista* Next) : 
+	info{ Info }, prev{ Prev }, next{ Next } {
+}
+
+ListaEntidades::ElementoLista::~ElementoLista() {}
+
+void ListaEntidades::ElementoLista::setInfo(Entidade* Info) {
+	info = Info;
+}
+
+Entidade* ListaEntidades::ElementoLista::getInfo() {
+	return info;
+}
+
+void ListaEntidades::ElementoLista::setPrev(ElementoLista* Prev) {
+	prev = Prev;
+}
+
+ListaEntidades::ElementoLista* ListaEntidades::ElementoLista::getPrev() {
+	return prev;
+}
+
+void ListaEntidades::ElementoLista::setNext(ElementoLista* Next) {
+	next = Next;
+}
+
+ListaEntidades::ElementoLista* ListaEntidades::ElementoLista::getNext() {
+	return next;
+}
+
---
+++ ListaEntidades.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Entidade.h"
+
+using namespace sf;
+
+class ListaEntidades {
+private:
+	class ElementoLista {
+	private:
+		ElementoLista* prev;
+		ElementoLista* next;
+		Entidade* info;
+	public:
+		ElementoLista(Entidade* Info = nullptr, ElementoLista* Prev = nullptr, ElementoLista* Next = nullptr);
+		~ElementoLista();
+
+		void setInfo(Entidade* Info);
+		Entidade* getInfo();
+		
+		void setPrev(ElementoLista* Prev);
+		ElementoLista* getPrev();
+
+		void setNext(ElementoLista* Next);
+		ElementoLista* getNext();
+
+
+	};
+
+	ElementoLista* current;
+	ElementoLista* head;
+	ElementoLista* tail;
+
+public:
+	ListaEntidades();
+	~ListaEntidades();
+
+	void insert(Entidade* info);
+	void deleteAll();
+	Entidade* moveHead();
+	Entidade* moveNext();
+
+	void atualizarEntidades(float t);
+	void drawAll( RenderWindow* window );
+	void destroyAll();
+
+
+};
